5a96c288e98c4e92194398df24d1285638b43fed
windchan <v20080443@163.com>
2017-02-01 21:01:37 -0800

    update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var router = require('./router');
+var router = require('./routes/router');
 
 app.use(router);
 
--- routes/router.js
+++ routes/router.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.send("rabbitshoe");
+    res.send("我老婆陈梦醒是世界上最可爱的宝宝！");
 });
 
 module.exports = router;
